3385258884cffb22f0085ac7ea25a13ce841c14e
CarmoAgilar <72893475+CarmoAgilar@users.noreply.github.com>
2021-01-07 19:56:27 -0600

    Examen III

---
+++ 3IV8-Examen-III-Carmona-Aguilar-Diego/src/buscaminas/CRUD.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Link del repositorio
+//https://github.com/CarmoAgilar/3IV8-Carmona-Aguilar-Diego
+package buscaminas;
+
+/**
+ *
+ * @author demon
+ */
+public class CRUD{
+    
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Lamma al metodo principal de la clase VentanaBuscaminas
+        VentanaBuscaminas mani = new VentanaBuscaminas();
+        
+        mani.principal();
+        
+    }
+}
